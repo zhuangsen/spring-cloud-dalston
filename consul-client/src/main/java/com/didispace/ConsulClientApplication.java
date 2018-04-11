ca580c4157fe0274e54b52d1f35d5300378f00d2
madison <zhuangsen@live.com>
2018-04-11 13:31:42 +0800

    spring cloud

---
+++ consul-client/src/main/java/com/didispace/ConsulClientApplication.java
@@ -0,0 +1,15 @@
+package com.didispace;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ConsulClientApplication {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(ConsulClientApplication.class).web(true).run(args);
+	}
+
+}
